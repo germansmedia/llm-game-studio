258c7ada270a5ea56b42ae1b7dfe4125d7956044
Desmond Germans <desmond@germansmedia.nl>
2023-12-21 16:36:14 +0100

    update

--- hello/hello.py
+++ hello/hello.py
@@ -1,40 +1,132 @@
-import random
 import json
+
+import random
+random.seed(1234)
+
 from openai import OpenAI
 client = OpenAI(base_url = 'http://localhost:1234/v1',api_key = 'key')
 
-# Adjust the following based on the model type
-# Alpaca style prompt format:
 prefix = "### Instruction:\n" 
 suffix = "\n### Response:"
 
-# 'Llama2 Chat' prompt format:
-#prefix = "[INST]"
-#suffix = "[/INST]"
+def complete(prompt,max_tokens=1500):
+    seed = random.randrange(0,10000)
+    response = client.completions.create(model='',prompt=f"{prefix}{prompt}{suffix}",max_tokens=max_tokens,seed=seed)
+    result = response.choices[0].text.strip('\n')
+    return result
 
-# This is a simple wrapper function to allow you simplify your prompts
-def complete(prompt):
-    formatted_prompt = f"{prefix}{prompt}{suffix}"
-    response = client.completions.create(model='',prompt=formatted_prompt,seed=12345)
-    return response.choices[0].text.strip('\n')
+def complete_seeded(prompt,seed,max_tokens=1500):
+    response = client.completions.create(model='',prompt=f"{prefix}{prompt}{suffix}",max_tokens=max_tokens,seed=seed)
+    result = response.choices[0].text.strip('\n')
+    return result
 
-random.seed(1234)
+def conceptify(text):
+    return text.strip('\n ').split('.')[0].strip(' .,:*-\"').lower()
 
-def objects_from_theme(theme):
-    count = random.randrange(4,8)
-    objects = complete(f"List {count} objects related to {theme}. Format as json array of single strings.")
-    return json.loads(objects)
+def verbalize_names(names):
+    result = ""
+    for i in range(len(names)):
+        if i == 0:
+            result = names[i]
+        elif i == len(names) - 1:
+            result = f"{result} and {names[i]}"
+        else:
+            result = f"{result}, {names[i]}"
+    return result
 
-theme = "blue"
+def generate_opposite(text):
+    text = complete(f"Give the opposite of {text}. One word only.",max_tokens=5)
+    return conceptify(text)
 
-print(f"Theme: {theme}")
+def generate_object(setting,theme):
+    object = complete(f"The setting is {setting}. Name one important everyday object related to {theme} in this setting. One word only.",max_tokens=5)
+    return conceptify(object)
 
-print("anchor objects:")
-print(objects_from_theme(theme))
+def generate_objects(setting,theme):
+    count = random.randrange(2,5)
+    objects = []
+    for i in range(count):
+        found = True
+        while found:
+            object = generate_object(setting,theme)
+            found = object in objects
+        objects.append(object)
+    return objects
 
-#print("story...")
-#story = complete(f"{objects_chunk}\nin one sentence, write a compelling and luscious story about a protagonist, an antagonist and these objects")
-#print(f"    {story}")
+def generate_gender():
+    gender = "male"
+    if random.randrange(0,10) >= 5:
+        gender = "female"
+    return gender
+
+def generate_name(prime,job,gender):
+    name = complete(f"{prime} Think of a {gender} name for a {job}. One word only.",max_tokens=5)
+    return conceptify(name).capitalize()
+
+def generate_characters(count,prime,job,object_names):
+    characters = []
+    for i in range(count):
+        gender = generate_gender()
+        found = True
+        while found:
+            name = generate_name(prime,job,gender)
+            found = False
+            for character in characters:
+                if character['name'] == name:
+                    found = True
+                    break
+        looks = complete(f"{prime} Describe in one sentence what {name} looks like.",max_tokens=50)
+        activity = complete(f"{prime} Describe in one sentence what particular thing {name} likes to do best.",max_tokens=50)
+        strength = complete(f"{prime} Specify in one sentence the particular strength, smart or power of {name}.",max_tokens=50)
+        weakness = complete(f"{prime} Specify in one sentence the particular weakness, or achilles heel of {name}.",max_tokens=50)
+        trauma = complete(f"{prime} Describe the main source of trauma that {name} had to endure.",max_tokens=50)
+        character = { 'name': name,'gender': gender,'looks': looks,'activity': activity,'strength': strength,'weakness': weakness,'trauma': trauma }
+        characters.append(character)
+    return characters
+
+# ====
+
+setting = "futuristic"
+theme = "winter"
+
+print(f"setting: {setting}")
+print(f"theme: {theme}")
+
+antitheme = generate_opposite(theme)
+print(f"antitheme: {antitheme}")
+
+objects = generate_objects(setting,theme)
+object_names = verbalize_names(objects)
+print(f"anchor objects: {object_names}")
+
+prime = f"The setting is {setting}. The theme is {theme}."
+antiprime = f"The setting is {setting}. The theme is {antitheme}."
+
+heroes = generate_characters(random.randrange(1,2),prime,"hero",object_names)
+print("heroes:")
+for character in heroes:
+    print(f"    {character['name']}")
+    print(f"        strength: {character['strength']}")
+    print(f"        weakness: {character['weakness']}")
+    print(f"        favorite activity: {character['activity']}")
+    print(f"        looks: {character['looks']}")
+    print(f"        trauma: {character['trauma']}")
+
+side_jobs = ['cleric','smith','driver','guard','fixer']
+sides = generate_characters(random.randrange(4,8),prime,side_jobs[random.randrange(0,len(side_jobs))],object_names)
+print("side characters:")
+for character in sides:
+    print(f"    {character['name']}")
+    print(f"        strength: {character['strength']}")
+    print(f"        weakness: {character['weakness']}")
+    print(f"        favorite activity: {character['activity']}")
+    print(f"        looks: {character['looks']}")
+    print(f"        trauma: {character['trauma']}")
+
+villains = generate_characters(random.randrange(3,6),antiprime,"villain",object_names)
+print("villains:")
+for character in villains:
+    print(f"    {character['name']}: {character['trauma']}")
 
 #print("protagonist...")
 #protagonist = complete(f"{story}\ndescribe the protagonist in one sentence")
